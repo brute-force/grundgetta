c9173bf68ee99c7714719b392b3205a1ed191bef
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-08-24 18:13:05 -0400

    add comments

--- api-requests.js
+++ api-requests.js
@@ -1,6 +1,11 @@
 const request = require('superagent');
 
-// retrieve the garbage pickup days
+/**
+ * Returns collection days and times for garbage pickups and recycling day for an address
+ * @param {string} numberAndStreet number and street name
+ * @param {number} zip zip code
+ * @return {Object} collections days for garbage and recycling, including garbage collection times
+ */
 const getData = async (numberAndStreet, zip) => {
   const uriDSNY = 'https://a827-donatenyc.nyc.gov/DSNYGeoCoder/api/DSNYCollection/CollectionSchedule?address=';
   const address = encodeURIComponent(`${numberAndStreet} ${zip}`);
@@ -20,6 +25,13 @@ const getData = async (numberAndStreet, zip) => {
   return { garbageDays, recyclingDay, residentialRoutingTime, commercialRoutingTime };
 };
 
+/**
+ * Validates time zone of Echo device against timeZone parameter via Alexa Settings API
+ * @param {string} apiAccessToken API access token for permissions lookup
+ * @param {string} deviceId device ID of device
+ * @param {string} timeZone time zone to compare against
+ * @return {boolean} whether device time zone is the same as timeZone parameter
+ */
 const isInCorrectTimezone = async (apiAccessToken, deviceId, timeZone) => {
   const uriTZ = `https://api.amazonalexa.com/v2/devices/${deviceId}/settings/System.timeZone`;
   const response = await request.get(uriTZ).set('Authorization', `Bearer ${apiAccessToken}`);
@@ -27,6 +39,10 @@ const isInCorrectTimezone = async (apiAccessToken, deviceId, timeZone) => {
   return response.body === timeZone;
 };
 
+/**
+ * Returns true if Sanitation holiday, false otherwise
+ * @return {boolean} whether current day is a Sanitation holiiday
+ */
 const isHolidaySchedule = async () => {
   const uriHolidaySchedule =
     'https://a827-donatenyc.nyc.gov/DSNYApi/api/Holidays/CheckSanitationHolidayToday';
