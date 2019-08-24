d3d8ed4ae8eb0896301189f77317bd4812871688
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-08-24 00:58:23 -0400

    refactor api requests -also allows stubbing device time zone lookup in tests

---
+++ api-requests.js
@@ -0,0 +1,35 @@
+const request = require('superagent');
+
+// retrieve the garbage pickup days
+const getData = async (numberAndStreet, zip) => {
+  const uriDSNY = 'https://a827-donatenyc.nyc.gov/DSNYGeoCoder/api/DSNYCollection/CollectionSchedule?address=';
+  const address = encodeURIComponent(`${numberAndStreet} ${zip}`);
+  const { body: { RegularCollectionSchedule, RecyclingCollectionSchedule: recyclingDay } } = await request
+    .get(`${uriDSNY}${address}`);
+
+  const garbageDays = RegularCollectionSchedule.split(',');
+
+  return { garbageDays, recyclingDay };
+};
+
+const isInCorrectTimezone = async (apiAccessToken, deviceId, timeZone) => {
+  const uriTZ = `https://api.amazonalexa.com/v2/devices/${deviceId}/settings/System.timeZone`;
+  const response = await request.get(uriTZ).set('Authorization', `Bearer ${apiAccessToken}`);
+
+  return response.body === timeZone;
+};
+
+const isHolidaySchedule = async () => {
+  const uriHolidaySchedule =
+    'https://a827-donatenyc.nyc.gov/DSNYApi/api/Holidays/CheckSanitationHolidayToday';
+
+  const { body: isHoliday } = await request.get(uriHolidaySchedule);
+
+  return isHoliday;
+};
+
+module.exports = {
+  getData,
+  isInCorrectTimezone,
+  isHolidaySchedule
+};
