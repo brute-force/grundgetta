d0c9da7773801f13656135a42e69a0d5884f4027
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-08-26 06:17:45 -0400

    allow custom error message

--- util/AddressNotFoundError.js
+++ util/AddressNotFoundError.js
@@ -1,6 +1,16 @@
+/**
+ * Constructs an AddressNotFoundError object with specified message.
+ *
+ * @classdesc
+ *
+ * Error for addresses not found in the DSNY database.
+ *
+ * @constructor
+ * @param {string} [message='Address not found in DSNY database.] error message
+ */
 class AddressNotFoundError extends Error {
-  constructor () {
-    super('Address not found in DSNY database.');
+  constructor (message = 'Address not found in DSNY database.') {
+    super(message);
     this.name = 'AddressNotFoundError';
     Error.captureStackTrace(this, AddressNotFoundError);
   }
