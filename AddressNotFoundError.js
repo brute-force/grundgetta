90171bb6887a00d99021396188e3c05e38832590
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-08-26 03:10:28 -0400

    moved to util/

--- AddressNotFoundError.js
+++
@@ -1,9 +0,0 @@
-class AddressNotFoundError extends Error {
-  constructor () {
-    super('Address not found in DSNY database.');
-    this.name = 'AddressNotFoundError';
-    Error.captureStackTrace(this, AddressNotFoundError);
-  }
-}
-
-module.exports = AddressNotFoundError;
